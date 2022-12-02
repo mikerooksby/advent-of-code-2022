c95bd1bfb9375bfefdee512296335239dfba4fd6
mikerooksby <mikerooksby@gmail.com>
2022-12-02 19:02:49 +0000

    Solve Day 1

---
+++ 01/index.js
@@ -0,0 +1,41 @@
+import fs from 'fs/promises';
+
+async function readInput(fileName) {
+  const file = await fs.readFile(fileName, 'utf-8');
+  return file.trim().split('\n');
+}
+
+async function getTotals(inputs) {
+  const totals = [];
+  let currentSum = 0;
+
+  inputs.forEach((input) => {
+    if (!input) {
+        totals.push(currentSum);
+        currentSum = 0;
+    } else {
+        currentSum += Number(input);
+    }
+  });
+  totals.push(currentSum);
+  return totals;
+}
+
+async function solvePartOne(fileName) {
+    const inputs = await readInput(fileName);
+    const totals = await getTotals(inputs);
+    return Math.max(...totals);
+}
+
+async function solvePartTwo(fileName) {
+    const inputs = await readInput(fileName);
+    const totals = await getTotals(inputs);
+    totals.sort((a, b) => a - b);
+    const total = [totals.pop(), totals.pop(), totals.pop()].reduce((sum, v) => sum + v);
+    return total;
+}
+
+const fileName = 'input.txt';
+const resultOne = await solvePartOne(fileName);
+const resultTwo = await solvePartTwo(fileName);
+console.log(resultOne, resultTwo);
